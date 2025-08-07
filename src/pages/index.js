09dc35d272800ccf6b55ff1fd6b46c89ef91c985
iicekarthik <iice.karthik@gmail.com>
2025-08-07 13:48:35 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start p-6">
 
         <Componentpage />
+        <h1>new branch</h1>
 
       </main>
  
